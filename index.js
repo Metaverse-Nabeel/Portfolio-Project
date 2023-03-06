a5a5fe82c9d4d781a43116412d698e1aef5436bb
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-03-07 01:27:01 +0500

    Corrected JS linter errors

--- index.js
+++ index.js
@@ -4,23 +4,21 @@ const navbarItems = document.querySelector('.navbarItems');
 const desktopNavbar = document.querySelector('.desktopNavbar');
 const navbar = document.querySelector('.navbar');
 const intro = document.querySelector('.intro');
-
-hamburger.addEventListener("click", function () {
-    navbarItems.classList.remove('hide');
-    navbarItems.classList.add('mobileNavbarItems');
-    desktopNavbar.classList.add('mobileNavbar');
-    navbar.classList.add('overlay');
-    hamburger.classList.add('hide');
-    closeBtn.classList.remove('hide');
-    intro.classList.add('blurred_intro');
+hamburger.addEventListener('click', () => {
+  navbarItems.classList.remove('hide');
+  navbarItems.classList.add('mobileNavbarItems');
+  desktopNavbar.classList.add('mobileNavbar');
+  navbar.classList.add('overlay');
+  hamburger.classList.add('hide');
+  closeBtn.classList.remove('hide');
+  intro.classList.add('blurred_intro');
 });
-
-closeBtn.addEventListener('click', function () {
-    navbarItems.classList.add('hide');
-    navbarItems.classList.remove('mobileNavbarItems');
-    desktopNavbar.classList.remove('mobileNavbar');
-    navbar.classList.remove('overlay');
-    hamburger.classList.remove('hide');
-    closeBtn.classList.add('hide');
-    intro.classList.remove('blurred_intro');
+closeBtn.addEventListener('click', () => {
+  navbarItems.classList.add('hide');
+  navbarItems.classList.remove('mobileNavbarItems');
+  desktopNavbar.classList.remove('mobileNavbar');
+  navbar.classList.remove('overlay');
+  hamburger.classList.remove('hide');
+  closeBtn.classList.add('hide');
+  intro.classList.remove('blurred_intro');
 });
