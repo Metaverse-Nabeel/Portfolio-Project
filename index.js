56c2aad26832b77b9ae6fa7c2d3d11f471013af4
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-03-07 00:57:28 +0500

    Corrected linter errors

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-let hamburger = document.querySelector(".hamburger");
-let closeBtn = document.querySelector(".closeBtn");
-let navbarItems = document.querySelector(".navbarItems");
-let desktopNavbar = document.querySelector(".desktopNavbar");
-let navbar = document.querySelector(".navbar");
-let intro= document.querySelector(".intro");
+const hamburger = document.querySelector('.hamburger');
+const closeBtn = document.querySelector('.closeBtn');
+const navbarItems = document.querySelector('.navbarItems');
+const desktopNavbar = document.querySelector('.desktopNavbar');
+const navbar = document.querySelector('.navbar');
+const intro = document.querySelector('.intro');
 
 hamburger.addEventListener("click", function () {
-    navbarItems.classList.remove("hide");
-    navbarItems.classList.add("mobileNavbarItems");
-    desktopNavbar.classList.add("mobileNavbar");
-    navbar.classList.add("overlay");
-    hamburger.classList.add("hide");
-    closeBtn.classList.remove("hide");
-    intro.classList.add("blurred_intro")
-})
+    navbarItems.classList.remove('hide');
+    navbarItems.classList.add('mobileNavbarItems');
+    desktopNavbar.classList.add('mobileNavbar');
+    navbar.classList.add('overlay');
+    hamburger.classList.add('hide');
+    closeBtn.classList.remove('hide');
+    intro.classList.add('blurred_intro');
+});
 
-closeBtn.addEventListener("click", function () {
-    navbarItems.classList.add("hide");
-    navbarItems.classList.remove("mobileNavbarItems");
-    desktopNavbar.classList.remove("mobileNavbar");
-    navbar.classList.remove("overlay");
-    hamburger.classList.remove("hide");
-    closeBtn.classList.add("hide");
-    intro.classList.remove("blurred_intro");
-})
-
+closeBtn.addEventListener('click', function () {
+    navbarItems.classList.add('hide');
+    navbarItems.classList.remove('mobileNavbarItems');
+    desktopNavbar.classList.remove('mobileNavbar');
+    navbar.classList.remove('overlay');
+    hamburger.classList.remove('hide');
+    closeBtn.classList.add('hide');
+    intro.classList.remove('blurred_intro');
+});
