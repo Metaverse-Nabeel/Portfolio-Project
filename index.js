4c6b4b243f8e336f9cf49349841d6be78adb7b39
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-03-08 20:26:51 +0500

    Add popups to see each project card in detail

--- index.js
+++ index.js
@@ -31,3 +31,96 @@ navbarItems.addEventListener('click', () => {
   closeBtn.classList.add('hide');
   intro.classList.remove('blurred_intro');
 });
+const data = [
+  {
+    projectTitle: 'Tonic',
+    imageURL: './Assets/images/portfolio-cards/portfolio_card1_desktop.svg',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+    technologies: ['HTML', 'CSS', 'JS', 'Ruby on Rails'],
+    seeLive: 'https://metaverse-nabeel.github.io/Portfolio-Project/',
+    sourceCode: 'https://github.com/Metaverse-Nabeel/Portfolio-Project',
+  },
+  {
+    projectTitle: 'Multi-Post Stories',
+    imageURL: './Assets/images/portfolio-cards/portfolio_card2_desktop.svg',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+    technologies: ['HTML', 'CSS', 'JS', 'Ruby on Rails'],
+    seeLive: 'https://metaverse-nabeel.github.io/Portfolio-Project/',
+    sourceCode: 'https://github.com/Metaverse-Nabeel/Portfolio-Project',
+  },
+  {
+    projectTitle: 'Facebook 360',
+    imageURL: './Assets/images/portfolio-cards/portfolio_card3_desktop.svg',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+    technologies: ['HTML', 'CSS', 'JS', 'Ruby on Rails'],
+    seeLive: 'https://metaverse-nabeel.github.io/Portfolio-Project/',
+    sourceCode: 'https://github.com/Metaverse-Nabeel/Portfolio-Project',
+  },
+  {
+    projectTitle: 'Uber Navigation',
+    imageURL: './Assets/images/portfolio-cards/portfolio_card4_desktop.svg',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+    technologies: ['HTML', 'CSS', 'JS', 'Ruby on Rails'],
+    seeLive: 'https://metaverse-nabeel.github.io/Portfolio-Project/',
+    sourceCode: 'https://github.com/Metaverse-Nabeel/Portfolio-Project',
+  },
+];
+document.querySelectorAll('.clickBtn').forEach((occurence) => {
+  const id = occurence.getAttribute('id');
+  const popupCard = `
+<div class="project1" id="project1">
+      <div class="popupTop">
+          <div class="projectPopupTitle">
+            <div class="titlePopup">
+              <h3 class="projectTitle">${data[id].projectTitle}</h3>
+              <img
+              id="popupClose${id}"
+              class="popupClose"
+              src="./Assets/images/popupCancel.svg"
+              alt="closeBtn"
+              />
+            </div>
+            <ul class="clientDetail">
+              <li class="clientName">CANOPY</li>
+              <li><img src="./Assets/images/dot.svg" alt="dot"/></li>
+              <li>Back End Dev</li>
+              <li><img src="./Assets/images/dot.svg" alt="dot"/></li>
+              <li>2015</li>
+            </ul>
+          </div>
+          <img class="projectImage" src=${data[id].imageURL} alt="projectImage">
+        <div class="popupDown">
+        <div class="projectDesc popupDesc">${data[id].description}</div>
+          <div class="popupBtns">
+            <ul class="projectTags">
+              <li class="tag">${data[id].technologies[0]}</li>
+              <li class="tag">${data[id].technologies[1]}</li>
+              <li class="tag">${data[id].technologies[2]}</li>
+              <li class="tag">${data[id].technologies[3]}</li>
+            </ul>
+            <hr>
+            <div class="actionBtnPopup">
+<a class="btn btnPopup" href=${data[id].seeLive} target='blank'>
+                See Live <img class="seeAll" src='./Assets/images/seeLive.svg' alt="seeLive"></a>
+            <a class="btn btnPopup" href=${data[id].sourceCode} target='blank'>
+            See Source <img class="seeAll" src='./Assets/images/githubCode.svg' alt="githubCode"></a>
+            </div>
+          </div>
+        </div>
+        </div>  
+    </div>
+  </div>`;
+  occurence.addEventListener('click', () => {
+    const body = document.getElementById('body');
+    const section = document.createElement('section');
+    section.innerHTML = popupCard;
+    section.classList.add('hide');
+    body.appendChild(section);
+    section.classList.add('projectPopup');
+    if (section.classList.contains('hide')) { section.classList.remove('hide'); }
+    const hidePopup = document.getElementById(`popupClose${id}`);
+    hidePopup.addEventListener('click', () => {
+      body.removeChild(section);
+    });
+  });
+});
